9c74e266f8876f6d36a672f615c0193d8caef509
Breonna Corbett <breonnar@email.unc.edu>
2021-03-01 00:02:24 -0500

    project

---
+++ exercises/ex01/cyoa.py
@@ -0,0 +1,61 @@
+def snow_plans():
+            """help"""
+            print(" How about we build a snowman?")
+            Answer = input("Enter Yes or No:")
+            response = Answer
+            if response == "Yes":
+                print("great")
+            else:
+                print("oh no oh no")
+
+
+
+if todays_forecast == "snowy" :
+        print(f"\U0001F427 wants to know waddle do today.")
+        def snow_plans():
+            """help"""
+            print(" How about we build a snowman?")
+            Answer = input("Enter Yes or No:")
+            points = 0
+            response = Answer
+            if response == "Yes":
+                print("great")
+                points = points + 15 
+            else:
+                print("oh no oh no")
+
+
+    def decisions_decisions():
+    """What activity will you choose?"""
+    print (f"\U0001F427 wants to know waddle do today!")
+
+    decisions_decisions()
+
+     def snow_plans():
+            """help"""
+
+
+    print(" How about we build a snowman?")
+        answer = input("Enter Yes or No:")
+        points = 0
+        response = answer
+        if response == "Yes":
+                print("great")
+        points = points + 15 
+        else:
+            print("oh no oh no")
+
+            def snow_plans():
+    """What will you do while it is snowing?"""
+    while ( todays_forecast == "snowy"):
+        input( "Do you want to: A) Build a snowman and drink hot chocolate. B) Go swimming and drink iced tea. [A/B] ? :"
+
+
+
+def main() -> None:
+    snow_plans()
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ exercises/ex01/p_w.py
@@ -0,0 +1,28 @@
+
+
+import random
+weather : str = ["snowy", "sunny", "rainy", "windy"]
+todays_forecast = random.choice(weather)
+print(f"The weather today is {todays_forecast}!")
+
+points : int = "Adventure Points"
+
+if todays_forecast == "snowy" :
+        print(f"\U0001F427 wants to know waddle do today.")       
+else:
+    if todays_forecast == "sunny" : 
+        print(f"\U0001F427 wants to know waddle do today.")
+    if todays_forecast == "rainy" :
+        print(f"\U0001F427 wants to know waddle do today.")
+    if todays_forecast == "windy":
+        print(f"\U0001F427 wants to know waddle do today.")
+
+ 
+def main() -> None:
+
+
+    if __name__ == "__main__":
+        main()
+
+        while True 
+        if yes call morning()
