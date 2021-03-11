14c0ee7bd24fb585e3c5122efd1b76f4854bd520
Breonna Corbett <breonnar@email.unc.edu>
2021-03-11 18:43:17 -0500

    hvc

--- exercises/ex03/avoid_fifth.py
+++ exercises/ex03/avoid_fifth.py
@@ -1,13 +1,28 @@
 """EX03 - avoid_fifth function."""
 
-__author__: str = "YOUR PID HERE"
+__author__: str = "730327440"
+
+
+def avoid_fifth (word: str) -> str :
+    """Whenever strings have E or e, produce a new string them."""
+    new_str: str = "" 
+    i: int = 0
+    while i < len(word):
+        if word[i] != "E" and word[i] != "e" :
+            new_str= new_str + word[i]
+        i+=1
+    return new_str
+
+
+
 
 
 def main() -> None:
     """Entrypoint of the program."""
     # Put print statements here to test your function
     # ex. print(avoid_fifth("hello there"))
-
+    avoid_fifth("str")
+    print(avoid_fifth("HELLO, my dear friend"))
 
 if __name__ == "__main__":
     main()
--- exercises/ex03/primes.py
+++ exercises/ex03/primes.py
@@ -1,13 +1,37 @@
 """EX03 - prime functions."""
 
-__author__: str = "YOUR PID HERE"
+__author__: str = "730327440"
+
+def its_prime(n: int) -> bool:
+    """Figuring out if a number is prime or not""" 
+    i: int = 2
+    while n > 1:
+        if (n % i) == 0:
+            return False
+        elif (n % i) != 0:
+            return True
+        i=+1
+    return False
+    
+
+
+        
+
+
+
+
+
+
+
+
 
 
 def main() -> None:
     """Entrypoint of the program."""
     # Put print statements here to test your function
     # ex. print(is_prime(5)), print(list_primes(10, 20))
-
+    its_prime(1)
+    print(its_prime(4))
 
 if __name__ == "__main__":
     main()
