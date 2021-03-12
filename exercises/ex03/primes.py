222c6bf816d662305c160bd0cd6befd6a52da31c
Breonna Corbett <breonnar@email.unc.edu>
2021-03-11 20:46:23 -0500

    n

--- exercises/ex03/primes.py
+++ exercises/ex03/primes.py
@@ -5,10 +5,10 @@ __author__: str = "730327440"
 def its_prime(n: int) -> bool:
     """Figuring out if a number is prime or not""" 
     i: int = 2
-    while n > 1:
+    while n >= 1:
         if (n % i) == 0:
             return False
-        elif (n % i) != 0:
+        else:
             return True
         i=+1
     return False
@@ -31,7 +31,7 @@ def main() -> None:
     # Put print statements here to test your function
     # ex. print(is_prime(5)), print(list_primes(10, 20))
     its_prime(1)
-    print(its_prime(4))
+    print(its_prime(17))
 
 if __name__ == "__main__":
     main()
