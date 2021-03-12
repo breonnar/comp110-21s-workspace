aa51d3b0406731df9c2bc1db5cba811fe7de750c
Breonna Corbett <breonnar@email.unc.edu>
2021-03-11 23:43:42 -0500

    okiii

--- exercises/ex03/primes.py
+++ exercises/ex03/primes.py
@@ -4,7 +4,7 @@
 __author__: str = "730327440"
 
 
-def is_prime(n: int) -> bool:
+def is_primes(n: int) -> bool:
     """Figuring out if a number is prime or not.""" 
     i: int = 2
     while i < n:
@@ -16,16 +16,24 @@ def is_prime(n: int) -> bool:
 
 def list_primes(a: int, b: int) -> list[int]:
     """The list for primes."""
-
+    plist: list[int] = []
+    for x in range(a,b):
+        if is_primes(x):
+            plist.append(x)
+    return plist
 
 def main() -> None:
     """Entrypoint of the program."""
     # Put print statements here to test your function
     # ex. print(is_prime(5)), print(list_primes(10, 20))
-    is_prime(2)
-    print(is_prime(16))
+    is_primes(2)
+    print(is_primes(16))
+    print(list_primes(3, 7))
+    print(list_primes(10, 20))
+    print(list_primes(25, 28))
+    print(list_primes(-1, 5))
 
 
 if __name__ == "__main__":
     main()
-    is_prime(1)
+    is_primes(1)
