36039a94b1fa0fc4ae570a5dd48e38691c0153ac
Breonna Corbett <breonnar@email.unc.edu>
2021-04-08 21:28:35 -0400

    yayyyy

--- exercises/ex04/data_utils.py
+++ exercises/ex04/data_utils.py
@@ -10,7 +10,7 @@ def read_csv_rows(csv_file: str) -> list[dict[str, str]]:
     """Read a CSV file's contents into a list of rows."""
 # TODO 0.1) Complete the implementation of this function here.
     data: list[dict[str, str]] = []
-    file_handle = open(csv_file,"r", encoding="utf8")
+    file_handle = open(csv_file, "r", encoding="utf8")
     csv_reader = DictReader(file_handle)
     for row in csv_reader:
         data.append(row)
@@ -20,22 +20,24 @@ def read_csv_rows(csv_file: str) -> list[dict[str, str]]:
 
 # TODO: Define the other functions here.
 def column_values(table: list[dict[str, str]], names: str) -> list[str]:
+# column values names turning into a list
     col_val: list[str] = []
-    for all_row in table:
-       col_val.append(all_row[names])
+        for all_row in table:
+        col_val.append(all_row[names])
     return col_val
         
 
 def columnar(co: list[dict[str,str]]) -> dict[str,list[str]]:
-    #first_kv is all of the names in code at index 0, new dict[column name]: make it equal to the list of values of age 
-    new_dict: dict[str,list[str]] = {}
+# first_kv is all of the names in code at index 0, new dict[column name]: make it equal to the list of values of age 
+    new_dict: dict[str, list[str]] = {}
     for first_kv in co[0]: 
-        new_dict[first_kv] = column_values(co,first_kv)
+        new_dict[first_kv] = column_values(co, first_kv)
     return new_dict
     
 
-def head(cb_table:dict[str, list[str]], row_num: int) -> dict[str,list[str]]:
-    head_dict: dict[str,list[str]] = {}
+def head(cb_table:dict[str, list[str]], row_num: int) -> dict[str, list[str]]:
+# making a new column based table with int
+    head_dict: dict[str, list[str]] = {}
     for each_col in cb_table:
         list_str = []
         for i in range(0, row_num):
@@ -46,16 +48,19 @@ def head(cb_table:dict[str, list[str]], row_num: int) -> dict[str,list[str]]:
 
 
 def select(cb2_table: dict[str, list[str]], new_cols: list[str]) -> dict[str, list[str]]: 
+# make a new column with certain cols from original cols
     new_dict = {}
     for cols in new_cols:
         new_dict[cols] = cb2_table[cols]
     return new_dict
 
-def count(oki: list[str]) -> dict[str,int]:
+
+def count(oki: list[str]) -> dict[str, int]:
+#associating keys with the amount of time their values appeared 
     built = {}
     for elements in oki: 
         if elements in built: 
-            built[elements]+=1
+            built[elements] += 1
         else: 
             built[elements] = 1
     return built 
