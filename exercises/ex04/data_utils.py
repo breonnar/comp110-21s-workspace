b49706d0dcca0d7f2a4b2b23929c5436fba6b63f
Breonna Corbett <breonnar@email.unc.edu>
2021-04-08 21:37:56 -0400

    yayaya

--- exercises/ex04/data_utils.py
+++ exercises/ex04/data_utils.py
@@ -20,15 +20,15 @@ def read_csv_rows(csv_file: str) -> list[dict[str, str]]:
 
 # TODO: Define the other functions here.
 def column_values(table: list[dict[str, str]], names: str) -> list[str]:
-# column values names turning into a list
+    """Column values names turning into a list."""
     col_val: list[str] = []
         for all_row in table:
         col_val.append(all_row[names])
     return col_val
         
 
-def columnar(co: list[dict[str,str]]) -> dict[str,list[str]]:
-# first_kv is all of the names in code at index 0, new dict[column name]: make it equal to the list of values of age 
+def columnar(co: list[dict[str, str]]) -> dict[str, list[str]]:
+"""First_kv is all of the names in code at index 0, new dict[column name]: make it equal to the list of values of age."""
     new_dict: dict[str, list[str]] = {}
     for first_kv in co[0]: 
         new_dict[first_kv] = column_values(co, first_kv)
@@ -36,7 +36,7 @@ def columnar(co: list[dict[str,str]]) -> dict[str,list[str]]:
     
 
 def head(cb_table:dict[str, list[str]], row_num: int) -> dict[str, list[str]]:
-# making a new column based table with int
+    """Making a new column based table with int."""
     head_dict: dict[str, list[str]] = {}
     for each_col in cb_table:
         list_str = []
@@ -48,7 +48,7 @@ def head(cb_table:dict[str, list[str]], row_num: int) -> dict[str, list[str]]:
 
 
 def select(cb2_table: dict[str, list[str]], new_cols: list[str]) -> dict[str, list[str]]: 
-# make a new column with certain cols from original cols
+    """Make a new column with certain cols from original columns."""
     new_dict = {}
     for cols in new_cols:
         new_dict[cols] = cb2_table[cols]
@@ -56,7 +56,7 @@ def select(cb2_table: dict[str, list[str]], new_cols: list[str]) -> dict[str, li
 
 
 def count(oki: list[str]) -> dict[str, int]:
-#associating keys with the amount of time their values appeared 
+    """Associating keys with the amount of time their values appeared."""
     built = {}
     for elements in oki: 
         if elements in built: 
